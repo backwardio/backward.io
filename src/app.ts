b994ee4ad4bb586dc0b85ee2299cd9948873e468
angular-cli <angular-cli@angular.io>
2015-11-27 15:19:46 +0200

    chore: initial commit from angular-cli v0.0.0

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/angular2';
+import {BkwdApp} from './app/bkwd';
+
+
+bootstrap(BkwdApp);
---
+++ src/app/bkwd.ts
@@ -0,0 +1,15 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+  selector: 'bkwd-app',
+  providers: [],
+  templateUrl: 'app/bkwd.html',
+  directives: [],
+  pipes: []
+})
+export class BkwdApp {
+
+  constructor() {}
+
+}
